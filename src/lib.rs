a20605cd809ebe19bedd7151e7965caa13e84bdd
JPenn01 <jayden.penn@outlook.com>
2026-09-23 14:52:14 +0000

    fix: changed values to intergers,  changed mut to 0, and added = to line 36.

--- src/lib.rs
+++ src/lib.rs
@@ -31,9 +31,9 @@
 ///
 /// assert_eq!(sum_to(10), 55);
 /// ```
-pub fn sum_to(n: u32) -> u32 {
-    let mut total = 1;
-    for i in 1..n {
+pub fn sum_to(n: i32) -> i32 {
+    let mut total = 0;
+    for i in 1..=n {
         total += i;
     }
     total
